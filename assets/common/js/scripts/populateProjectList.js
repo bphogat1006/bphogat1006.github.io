9544d7817e6c3ec3431dbda25b457ee31fb4dfe1
bphogat1006 <b.phogat1925@gmail.com>
2021-03-02 19:34:29 -0500

    automate project list population

---
+++ assets/common/js/scripts/populateProjectList.js
@@ -0,0 +1,12 @@
+
+var projects = document.getElementsByClassName("project-list-item")
+var projectsContainer = document.getElementById("project-list")
+var a = projectInfo
+
+function createProjectItem(html, title, description) {
+
+}
+
+for(var i=0; i < projects.length-1; i++) {
+  projects[i].style.borderBottom="2px solid black"
+}
---
+++ assets/common/js/scripts/projectInfo.js
@@ -0,0 +1,40 @@
+var projectInfo = [
+  {
+    name: "branching-vine",
+    title: "Branching Vine",
+    colorTheme: "#ffffff",
+    tags: [
+      "natural",
+      "animation",
+      "interactive"
+    ]
+  },
+  {
+    name: "dijkstra",
+    title: "Dijkstra Path Finder",
+    colorTheme: "#ffffff",
+    tags: [
+      "algorithm",
+      "interactive"
+    ]
+  },
+  {
+    name: "fractal-explorer",
+    title: "Fractal Explorer",
+    colorTheme: "#ffffff",
+    tags: [
+      "math",
+      "algorithm",
+      "interactive"
+    ]
+  },
+  {
+    name: "perlin-wave",
+    title: "Perlin Wave",
+    colorTheme: "#ffffff",
+    tags: [
+      "natural",
+      "math"
+    ]
+  }
+]
