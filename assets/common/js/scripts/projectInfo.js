ee2e4b0c4ef665c19604768a00b9df3d15b120d6
bphogat1006 <b.phogat1925@gmail.com>
2021-03-27 12:49:05 -0400

    added buttons to project subpage

--- assets/common/js/scripts/projectInfo.js
+++ assets/common/js/scripts/projectInfo.js
@@ -3,6 +3,7 @@ var projectInfo = [
     name: "fractal-explorer",
     title: "Fractal Explorer",
     description: "Utilizes a mathematical method of creating fractals, called the \"<a href='https://en.wikipedia.org/wiki/Chaos_game' target='_blank'>chaos game</a>,\" to create fractals with different polygon frames.",
+    extraInfo: "",
     colorTheme: "rgb(255,0,160)",
     tags: [
       "natural",
@@ -14,6 +15,7 @@ var projectInfo = [
     name: "branching-vine",
     title: "Branching Vine",
     description: "Uses the Perlin Noise technique to generate a natural looking vine pattern.",
+    extraInfo: "",
     colorTheme: "rgb(0,240,0)",
     tags: [
       "interactive",
@@ -24,6 +26,7 @@ var projectInfo = [
     name: "perlin-wave",
     title: "Perlin Wave",
     description: "An animation that demonstrates the Perlin Noise function.",
+    extraInfo: "",
     colorTheme: "rgb(0,190,170)",
     tags: [
       "natural",
@@ -35,6 +38,7 @@ var projectInfo = [
     name: "dijkstra",
     title: "Dijkstra Path Finder",
     description: "A web of dots is generated. The program uses Dijkstra's pathfinding algorithm to find the shortest path between any two nodes.",
+    extraInfo: "",
     colorTheme: "rgb(66,66,255)",
     tags: [
       "algorithm",
@@ -45,6 +49,7 @@ var projectInfo = [
     name: "sunflower",
     title: "Sunflower Disk",
     description: "The <a href='https://en.wikipedia.org/wiki/Golden_ratio' target='_blank'>Golden Ratio</a> is used to create a pattern such as those seen in the disk florets of flowers in the Asteraceae family, like <a href='https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Helianthus_whorl.jpg/440px-Helianthus_whorl.jpg' target='_blank'>sunflowers</a>.",
+    extraInfo: "",
     colorTheme: "rgb(237, 235, 0)",
     tags: [
       "natural",
@@ -55,3 +60,13 @@ var projectInfo = [
 ]
 
 // <a href='LINK_HERE' target='_blank'>TEXT_HERE</a>
+
+function getProject(query) {
+  var project = projectInfo[0]
+  projectInfo.forEach(function(proj) {
+    if(proj.name === query || proj.title === query) {
+      project = proj
+    }
+  })
+  return project
+}
---
+++ assets/common/js/scripts/projectPage.js
@@ -0,0 +1,35 @@
+
+var projName = location.href.split("name=")[1]
+var thisProject = getProject(projName)
+
+// set page title
+document.getElementById("projectTitle").innerHTML = thisProject.title
+
+// load p5 script
+var script = document.createElement('script')
+script.src = "../js/p5/"+projName+"/sketch.js"
+document.head.appendChild(script);
+
+// edit buttons
+var buttons = document.getElementsByTagName('i')
+console.log(buttons.length)
+for(var i=0; i < buttons.length; i++) {
+  // change color
+  buttons[i].style.color = thisProject.colorTheme
+}
+// home button
+document.getElementById("home").onclick = function() {
+  window.history.back()
+}
+// info button
+document.getElementById("info").onclick = function() {
+
+}
+// refresh buton
+document.getElementById("refresh").onclick = function() {
+  location.reload()
+}
+// src code button
+document.getElementById("source_code").onclick = function() {
+  window.open(script.src)
+}
